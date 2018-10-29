e695488b01de03ff203e032eb83ebf1481b4c786
Anastasiia Chepka <anastasiiac@wix.com>
2018-10-29 18:34:22 +0200

    Move access token logic to app component

--- src/app/app-component.tsx
+++ src/app/app-component.tsx
@@ -1,7 +1,55 @@
 import * as React from "react";
 import {History} from 'history';
 
+import { 
+    persistAccessToken, 
+    accessTokenIsExpired } 
+    from './utils/services/handle-token';
+import {getAccessToken} from './utils/remote-api/auth-api';
 //import {Route, Router, Switch} from 'react-router-dom';
 import {ProductList} from './containers';
+import { AppStore } from "./store";
+import {Provider as ReduxProvider} from 'react-redux';
+export interface AppComponentProps {
+    store: AppStore;
+}
+export interface AppComponentState {
+    hasAccessToken: boolean;
+ }
+export class App extends React.Component<AppComponentProps, AppComponentState> {
+    constructor(props: any) {
+        super(props);
 
-export const App: React.StatelessComponent<{}> = () => (<ProductList/>)
+        this.state = {
+            hasAccessToken: false
+        };
+    }
+
+    async getAccessToken() {
+        if  (accessTokenIsExpired()) {
+            const authenticate = await getAccessToken();
+            persistAccessToken(authenticate.access_token, authenticate.expires);
+        } else {
+            console.log("there is already token in the storage");
+        }
+
+        this.setState({
+            hasAccessToken: true
+        });
+    }
+
+    componentDidMount() {
+        this.getAccessToken();
+    }
+
+    render() {
+        const {hasAccessToken} = this.state;
+        const {store} = this.props;
+
+        return hasAccessToken && (
+            <ReduxProvider store={store}>
+                <ProductList />
+            </ReduxProvider>
+        );
+    }
+}
--- src/app/app-controller.tsx
+++ src/app/app-controller.tsx
@@ -1,67 +1,20 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import {render} from 'react-dom';
 
-import {App} from "./app-component";
-import { 
-    persistAccessToken, 
-    accessTokenIsExpired } 
-    from './utils/services/handle-token';
-import {getAccessToken} from './utils/remote-api/auth-api';
+import {App} from './app-component';
+
 declare let module: any;
 
- import {createAppStore, defaultState, AppStore} from './store';
- import {Provider as ReduxProvider} from 'react-redux';
- import {fetchProducts} from './store';
- export interface AppStarterState {
-    hasAccessToken: boolean;
- }
- export class AppStarter extends React.Component<{}, AppStarterState>{
+ import {createAppStore, defaultState} from './store';
 
-    constructor(props: any) {
-        super(props);
-
-        this.state = {
-            hasAccessToken: false
-        };
-    }
-
-    async getAccessToken() {
-        if  (accessTokenIsExpired()) {
-            const authenticate = await getAccessToken();
-            persistAccessToken(authenticate.access_token, authenticate.expires);
-        } else {
-            console.log("there is already token in the storage");
-        }
-
-        this.setState({
-            hasAccessToken: true
-        });
-    }
-
-    componentDidMount() {
-        this.getAccessToken();
-    }
-
-    render() {
-        const {hasAccessToken} = this.state;
-        const store = createAppStore(
-            defaultState
-        );
-
-        return hasAccessToken && (
-            <ReduxProvider store={store}>
-                <App/>
-            </ReduxProvider>
-        );
-    }
+ export const AppStarter = () => {
+    const store = createAppStore(defaultState);
+    return (<App store={store} />);
 }
 
-ReactDOM.render(
-    <AppStarter/>,
-    document.getElementById("root")
-);
+const rootNode = document.getElementById("root");
+render(<AppStarter />, rootNode);
 
 if (module.hot) {
     module.hot.accept();
  }
-
